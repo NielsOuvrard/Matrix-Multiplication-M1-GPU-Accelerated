34f24816e2f793bb09ac09f77a686fafc447e9ed
Niels Ouvrard <niels.ouvrard@epitech.eu>
2024-11-02 10:54:20 -0600

    in stack matrices OK

--- MetalComputeBasic/main.m
+++ MetalComputeBasic/main.m
@@ -15,7 +15,42 @@ An app that performs a simple calculation on a GPU.
 #define SIZE_X_B 2
 #define SIZE_Y_B 3
 
-int main(int argc, const char * argv[]) {
+typedef struct st_matrix {
+    int *data;
+    int y;
+    int x;
+} st_matrix;
+
+void multiply_matrix(st_matrix *a, st_matrix *b, MetalAdder *adder, int *result)
+{
+    for (int i = 0; i < a->y; i++) {
+        for (int j = 0; j < b->x; j++) {
+            int tmp[b->y];
+            for (int k = 0; k < b->y; k++) {
+                tmp[k] = b->data[(k * b->x) + j];
+            }
+
+            // Create buffers to hold data
+            [adder prepareListInt:(a->data + (i * a->x)) :tmp :a->x];
+            
+            // Send a command to the GPU to perform the calculation.
+            [adder sendComputeCommand:a->x :&(result[(i * a->y) + j])];
+        }
+    }
+}
+
+void print_matrix(st_matrix *matrix)
+{
+    for (int i = 0; i < matrix->x; i++) {
+        for (int j = 0; j < matrix->y; j++) {
+            printf("%d ", matrix->data[(i * matrix->y) + j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
         
         id<MTLDevice> device = MTLCreateSystemDefaultDevice();
@@ -35,37 +70,20 @@ int main(int argc, const char * argv[]) {
             {1, 2, 3},
             {4, 5, 6}
         };
+        st_matrix matrix_a = {(int *)a, SIZE_Y_A, SIZE_X_A};
         int b[SIZE_Y_B][SIZE_X_B] = {
             {7, 8},
             {9, 10},
             {11, 12}
         };
+        st_matrix matrix_b = {(int *)b, SIZE_Y_B, SIZE_X_B};
 
-        int multiplied[SIZE_Y_A][SIZE_X_B];
+        int multiplied[matrix_a.y][matrix_b.x];
+        multiply_matrix(&matrix_a, &matrix_b, adder, (int *)multiplied);
 
 
-        for (int i = 0; i < SIZE_Y_A; i++) {
-            for (int j = 0; j < SIZE_X_B; j++) {
-                int tmp[SIZE_Y_B];
-                for (int k = 0; k < SIZE_Y_B; k++) {
-                    tmp[k] = b[k][j];
-                }
+        print_matrix(&(st_matrix){(int *)multiplied, matrix_a.y, matrix_b.x});
 
-                // Create buffers to hold data
-                [adder prepareListInt:a[i] :tmp :SIZE_X_A];
-                
-                // Send a command to the GPU to perform the calculation.
-                [adder sendComputeCommand:SIZE_X_A :&(multiplied[i][j])];
-            }
-        }
-
-        // print
-        for (int i = 0; i < SIZE_Y_A; i++) {
-            for (int j = 0; j < SIZE_X_B; j++) {
-                printf("%d ", multiplied[i][j]);
-            }
-            printf("\n");
-        }
         NSLog(@"Execution finished");
     }
     return 0;
